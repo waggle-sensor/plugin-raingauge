bdac632a20a32a0a97240057958878abbeba46f9
Joe Swantek <joseph.swantek@northwestern.edu>
2021-08-26 12:59:13 -0500

    SAGE-1012: only accept metric values
    Remove converstion of imperial to metric and update unit testing
    accordingly.

--- parse.py
+++ parse.py
@@ -4,22 +4,16 @@ import logging
 logger = logging.getLogger(__name__)
 
 valid_keys = {"Acc", "EventAcc", "TotalAcc", "RInt"}
-
-unit_scale = {
-    "mm": 1.0,
-    "in": 25.4,
-    "mmph": 1.0,
-    "iph": 25.4,
-}
+valid_units = {"mm", "mmph"}
 
 
 def parse_values(s: str) -> typing.Dict[str, float]:
-    """Returns the sensor values converted to metric units."""
+    """Parses the sensor output only accepting metric units."""
     values = {}
     # line format looks like:
     # metric units:
     # Acc  0.00 mm, EventAcc  0.00 mm, TotalAcc  1.11 mm, RInt  0.00 mmph
-    # imperial units:
+    # imperial units (skipped):
     # Acc 0.000 in, EventAcc 0.000 in, TotalAcc 0.000 in, RInt 0.000 iph
 
     # split into individual key+value+units groups
@@ -28,27 +22,28 @@ def parse_values(s: str) -> typing.Dict[str, float]:
     for group in groups:
         fs = group.split()
 
-        # skip groups not of the form key+value+units
+        # skip groups not of the form key+value+units (ex "Acc 0.00 mm")
         if len(fs) != 3:
+            logger.warning(f"data format error [{fs}], skipping")
             continue
 
         key = fs[0]
         if key not in valid_keys:
+            logger.warning(f"unaccepted key [{fs[0]}], skipping value [{fs[1]}]")
+            continue
+
+        # only accept
+        if fs[2] not in valid_units:
+            logger.warning(f"unaccepted unit [{fs[2]}] skipping value [{fs[1]}]")
             continue
 
         # values *must* be numeric
         try:
             value = float(fs[1])
         except ValueError:
+            logger.warning(f"exception parsing value [{fs[1]}], skipping value")
             continue
 
-        try:
-            scale_factor = unit_scale[fs[2]]
-        except KeyError:
-            logger.warning("invalid unit %r", fs[2])
-            continue
-
-        # sensor precision is 2 decimal units
-        values[key] = round(value * scale_factor, 2)
+        values[key] = value
 
     return values
--- test_parse.py
+++ test_parse.py
@@ -1,39 +1,41 @@
 import parse
 import unittest
 
-in2mm = 25.4
-
 
 class TestParse(unittest.TestCase):
     def test_valid(self):
         r = parse.parse_values(
-            "Acc  0.00 mm, EventAcc  0.00 mm, TotalAcc  1.23 mm, RInt  0.00 mmph\n"
+            "Acc  1.23 mm, EventAcc  4.56 mm, TotalAcc  7.89 mm, RInt  2.48 mmph\n"
         )
         self.assertDictEqual(
-            r, {"Acc": 0.00, "EventAcc": 0.00, "TotalAcc": 1.23, "RInt": 0.00}
+            r, {"Acc": 1.23, "EventAcc": 4.56, "TotalAcc": 7.89, "RInt": 2.48}
         )
 
     def test_partial(self):
         r = parse.parse_values(
-            "cc  0.00 mm, EventAcc  0.00 mm, TotalAcc  1.23 mm, RInt  0.00 mmph\n"
+            "cc  1.23 mm, EventAcc  4.56 mm, TotalAcc  7.89 mm, RInt  2.48 mmph\n"
         )
-        self.assertDictEqual(r, {"EventAcc": 0.00, "TotalAcc": 1.23, "RInt": 0.00})
+        self.assertDictEqual(r, {"EventAcc": 4.56, "TotalAcc": 7.89, "RInt": 2.48})
 
     def test_glitch(self):
         r = parse.parse_values(
-            "AcQ  0.00 mm, EventAcc  0.x0 mm, TotalAcc  1.23 mm, RInt0.00 mmph\n"
+            "AcQ  1.23 mm, EventAcc  4.x6 mm, TotalAcc  7.89 mm, RInt2.48 mmph\n"
         )
-        self.assertDictEqual(r, {"TotalAcc": 1.23})
+        self.assertDictEqual(r, {"TotalAcc": 7.89})
 
     def test_num_fields(self):
-        r = parse.parse_values("0.00 mm, RInt 1.23 mmph\n")
-        self.assertDictEqual(r, {"RInt": 1.23})
+        r = parse.parse_values("7.89 mm, RInt 2.48 mmph\n")
+        self.assertDictEqual(r, {"RInt": 2.48})
 
-    def test_conversion(self):
+    def test_skip_inches(self):
         r = parse.parse_values("Acc  1.23 in\n")
-        self.assertDictEqual(r, {"Acc": round(1.23 * in2mm, 2)})
+        self.assertDictEqual(r, {})
         r = parse.parse_values("RInt 2.03 iph\n")
-        self.assertDictEqual(r, {"RInt": round(2.03 * in2mm, 2)})
+        self.assertDictEqual(r, {})
+        r = parse.parse_values(
+            "Acc  1.23 mm, EventAcc  4.56 in, TotalAcc  7.89 mm, RInt  2.48 iph\n"
+        )
+        self.assertDictEqual(r, {"Acc": 1.23, "TotalAcc": 7.89})
 
 
 if __name__ == "__main__":
